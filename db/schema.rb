f56de57529a8792511ad8855ef8ba9d71d8095bf
vagrant <gachapin5689@gmail.com>
2020-12-28 14:38:50 +0000

    20201228

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_26_141913) do
+ActiveRecord::Schema.define(version: 2020_12_28_141836) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
