8409fd478ca0d09ce6295454952377c5c6dd2991
vagrant <gachapin5689@gmail.com>
2020-12-29 14:04:23 +0000

    admin

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
+	# コールバック
+	# before_validation :set_nameless_name
+
 	validates :name, presence: true, length: { maximum: 30 }
 	validate :validate_name_not_including_conma
 
@@ -7,4 +10,9 @@ class Task < ApplicationRecord
 	def validate_name_not_including_conma
 		errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
 	end
+
+	# コールバック、「名前なし」を自動追加
+	# def set_nameless_name
+	# 	self.name = '名前なし' if name.blank?
+	# end
 end
