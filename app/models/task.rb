232e35d4f50d4e1fd5dec01d37c2512fc29b015b
vagrant <gachapin5689@gmail.com>
2020-12-30 06:50:09 +0000

    login

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,8 @@ class Task < ApplicationRecord
 	validates :name, presence: true, length: { maximum: 30 }
 	validate :validate_name_not_including_conma
 
+	belongs_to :user
+
 	private
 
 	def validate_name_not_including_conma
